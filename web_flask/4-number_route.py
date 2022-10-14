98f978bcf3721c2283b4c3dfaa79d4ff54f0a4ee
Adewole Michael <michaseyi@gmail.com>
2022-10-14 16:37:39 +0100

    Added task 4 web framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """
+    index is the handler for requests to the / route
+
+    :return (str): is Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    hbnb is the handler for requests to the /hbnb route
+
+    :return (str): is HBHB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """
+    c is the handler for request to the /c/<text> route
+
+    :param text(str): path vairable
+    :return (str): is C <text>
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text='is_cool'):
+    """
+    python is the handler for request to the /python/<text> route
+
+    :param text(str): path variable, default is 'is_cool'
+    :return (str): is Python <text>
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number(n: int):
+    """
+    number is the handler for request to the /number/<int:n> route
+
+    :param n(int): path variable
+    :return (str): is <n> is a number
+    """
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
