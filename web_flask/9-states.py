a2dacfb9442095b59b4609dc0a3a96445995a384
Adewole Michael <michaseyi@gmail.com>
2022-10-15 21:15:03 +0100

    Task 10 web framework

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,9 +10,9 @@ app.url_map.strict_slashes = False
 
 
 @app.route('/states')
-def states_list():
+def states():
     """
-    states_list renders a list of available states
+    states list of available states
     """
     from models.state import State
     from models import storage
@@ -20,25 +20,10 @@ def states_list():
     states.sort(key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
-
-@app.route('/cities_by_states')
-def cities_by_states():
-    """
-    cities_by_states renders a list of available states and cities
-    """
-    from models.state import State
-    from models import storage
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    for state in states:
-        state.cities.sort(key=lambda city: city.name)
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.route('/states/<id>')
-def states(id):
+def get_state(id):
     """
-    states displays a state and its cities
+    get_state displays a state and its cities
 
     :param id(str): is the id of the state to be desplayed
     """
